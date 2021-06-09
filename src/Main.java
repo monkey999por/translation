69d36c135c96c958314066b413f8371c749e704a
monkey999por <34706709+monkey999por@users.noreply.github.com>
2021-06-10 00:56:07 +0900

    add javadoc Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,38 @@ import setting.Setting;
 import thread.ClipBoardObserver;
 import thread.StandardInObserver;
 
+/**
+ * see sequence.drawio
+ */
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Setting.init(args[0]);
-		welcomePrint();
+    public static void main(String[] args) throws Exception {
+        Setting.init(args[0]);
+        welcomePrint();
 
-		// クリップボードを監視し、変更があった場合は翻訳者(TranslationWorker)を呼び出し 別スレッドで実行
-		new Thread(new ClipBoardObserver()).start();
+        // Monitor the clipboard and call the translator (Translation Worker)
+        // if there is a change and execute it in another thread.
+        new Thread(new ClipBoardObserver()).start();
 
-		// 標準入力を監視する。入力があった場合は翻訳者(TranslationWorker)を呼び出し 別スレッドで実行
-		new Thread(new StandardInObserver()).start();
+        // Monitor the standard in.
+        // if there is input and execute process in another thread.
+        new Thread(new StandardInObserver()).start();
 
-	}
+    }
 
-	private static void welcomePrint() {
-		System.out.println("------------------- Hello Translation -------------------");
-		System.out.println("@see: https://github.com/monkey999por/translation.git");
-		System.out.println("@see: https://github.com/monkey999por/language-detection.git");
-		System.out.println();
-		System.out.println("■ Setting: " + Setting.getFilePath());
-		Setting.printAll();
-		System.out.println();
-		System.out.println("■■■機能■■■");
-		System.out.println("  ・クリップボードを監視し、変更があった場合は翻訳します。");
-		System.out.println("  ・コンソールに入力した文字を翻訳します。");
-		System.out.println();
-	}
-
+    /**
+     * write welcome massage.
+     */
+    private static void welcomePrint() {
+        System.out.println("------------------- Hello Translation -------------------");
+        System.out.println("@see: https://github.com/monkey999por/translation.git");
+        System.out.println("@see: https://github.com/monkey999por/language-detection.git");
+        System.out.println();
+        System.out.println("■ Setting: " + Setting.getFilePath());
+        Setting.printAll();
+        System.out.println();
+        System.out.println("■■■機能■■■");
+        System.out.println("  ・クリップボードを監視し、変更があった場合は翻訳します。");
+        System.out.println("  ・コンソールに入力した文字を翻訳します。");
+        System.out.println();
+    }
 }
