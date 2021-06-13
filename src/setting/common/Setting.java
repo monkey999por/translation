9268734f4e190146b9d0474b29719162063727f0
monkey999por-sub <85392145+monkey999por-sub@users.noreply.github.com>
2021-06-13 11:08:55 +0900

    add checking as Setting.java

--- src/setting/common/Setting.java
+++ src/setting/common/Setting.java
@@ -1,5 +1,6 @@
 package setting.common;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import worker.CommandExecutor;
 
 import java.io.File;
@@ -9,6 +10,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -22,7 +24,7 @@ public class Setting {
 
     private static Properties properties;
 
-    // properties file path.
+    // current properties file path.
     private static String filePath;
 
     private Setting() {
@@ -53,7 +55,7 @@ public class Setting {
         try {
             properties.load(Files.newBufferedReader(Paths.get(propPath), StandardCharsets.UTF_8));
         } catch (IOException e) {
-            System.out.println(String.format("ファイルの読み込みに失敗しました。ファイル名:%s", propPath));
+            System.out.format("ファイルの読み込みに失敗しました。ファイル名:%s", propPath);
         }
     }
 
@@ -70,10 +72,17 @@ public class Setting {
      * same as {@link Properties#getProperty(String, String)}.
      *
      * @param key key
-     * @return matching key. if there is no matching key, return null.
+     * @return matching key. if there is no matching key, Application Stop.
      */
     private static String get(String key) {
-        return properties.getProperty(key, null);
+        isBlankKey(key);
+        String value = properties.getProperty(key, null);
+        if (Objects.isNull(value)) {
+            ApplicationFailure("key: " + key + "\r\ngetting value failed.");
+            return null;
+        } else {
+            return value;
+        }
     }
 
     /**
@@ -92,70 +101,86 @@ public class Setting {
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static String getAsString(String key) {
+        isBlankKey(key);
         return String.valueOf(get(key));
     }
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static Integer getAsInteger(String key) {
-        return Integer.valueOf(get(key));
+        // check
+        isBlankKey(key);
+        var value = get(key);
+        likeNumber(key, value);
+
+        return Integer.valueOf(value);
     }
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static Double getAsDouble(String key) {
-        return Double.valueOf(get(key));
+        // check
+        isBlankKey(key);
+        var value = get(key);
+        likeNumber(key, value);
+
+        return Double.valueOf(value);
     }
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static Long getAsLong(String key) {
-        return Long.valueOf(get(key));
+        // check
+        isBlankKey(key);
+        var value = get(key);
+        likeNumber(key, value);
+
+        return Long.valueOf(value);
     }
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static Boolean getAsBoolean(String key) {
-        var b = Boolean.valueOf(get(key));
-        if (b == null) {
-            System.out.println("can't convert to boolean. key is \"" + key + "\"");
-            return null;
-        } else {
-            return b;
-        }
+        isBlankKey(key);
+        var value = get(key.toLowerCase());
+        likeBoolean(key, value);
+
+        return Boolean.valueOf(value);
     }
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static File getAsFile(String key) {
+        isBlankKey(key);
         return new File(get(key));
     }
 
     /**
      * @param key same as {@link Setting#get(String)}
-     * @return setting type of convert to this.
+     * @return setting type of convert to this type.
      * @see Setting#get(String)
      */
     public static URL getAsURL(String key) {
+        isBlankKey(key);
         try {
             return new URL(get(key));
         } catch (MalformedURLException e) {
@@ -163,4 +188,56 @@ public class Setting {
         }
         return null;
     }
+
+    /**
+     * checking param is number??
+     *
+     * @param value checking target.
+     */
+    private static void likeNumber(String key, String value) {
+        isBlankKey(key);
+        if (!NumberUtils.isNumber(value)) {
+            ApplicationFailure("key : " + key + "\r\nthe property not Number like");
+        }
+    }
+
+    /**
+     * checking param like boolean??
+     *
+     * @param value checking target.
+     */
+    private static void likeBoolean(String key, String value) {
+        isBlankKey(key);
+        if (!(value.equals("true") || value.equals("false"))) {
+            ApplicationFailure("key : " + key + "\r\nvalue: " + value + "\r\nspecified \"true\" or \"false\"");
+        }
+    }
+
+    /**
+     * checking key is not null
+     *
+     * @param key key
+     */
+    private static void isBlankKey(String key) {
+        if (Objects.isNull(key) || key.equals("")) {
+            ApplicationFailure("key : " + key + "\r\nproperties get error. key is null");
+        }
+    }
+
+    /**
+     * Application. failed.
+     *
+     * @param detail Further message
+     */
+    private static void ApplicationFailure(String detail) {
+        System.out.println("application failed. \r\n" +
+                "It is possible that the settings are incorrect.\r\n" +
+                "properties path: " + filePath);
+        System.out.println();
+        if (!(Objects.isNull(detail) || detail.equals(""))) {
+            System.out.println(detail);
+        }
+        System.exit(-1);
+    }
 }
+
