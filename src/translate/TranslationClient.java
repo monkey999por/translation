7ce94337ab0fb588c7527d40a8a60d292f578b28
monkey999por <34706709+monkey999por@users.noreply.github.com>
2021-06-22 10:10:57 +0900

    add interface

--- src/translate/TranslationClient.java
+++ src/translate/TranslationClient.java
@@ -1,57 +1,6 @@
 package translate;
 
-import app.Setting;
-import com.cybozu.labs.langdetect.LangDetectException;
-import org.apache.commons.codec.net.URLCodec;
-import tools.Cmd;
-
-import java.io.UnsupportedEncodingException;
-import java.util.Objects;
-
-/**
- * see https://script.google.com/home/projects/1sFWfV_MOJbmBeTLT_R9F4Qq0qPolgkRsVt8A_sCMI2C9DTccZBRdHFDt/edit
- */
-public class TranslationClient {
-    private static final URLCodec codec = new URLCodec("UTF-8");
-
-    private LangDetector detector = new LangDetectorOfCybozuLabs();
-
-    public TranslationClient() throws LangDetectException {
-    }
-
-    /**
-     * create request URL as google apps script as "google_translate_api".
-     *
-     * @param text   translate text
-     * @param source before language
-     * @param target after language
-     * @return request URL as google apps script as "google_translate_api"
-     */
-    public static String createRequestUrl(String text, TargetLang source, TargetLang target) {
-        try {
-            text = Objects.isNull(text) ? "" : codec.encode(text, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            if (Setting.getAsBoolean("debug_mode")) {
-                e.printStackTrace();
-            }
-        }
-
-        String url = Setting.getAsString("translate_request_url");
-        return url.replace("{text}", text)
-                .replace("{source}", source.languageCode)
-                .replace("{target}", target.languageCode);
-    }
-
-    /**
-     * translate by URL.
-     *
-     * @param requestUrl created by {@link TranslationClient#createRequestUrl(String, TargetLang, TargetLang)}.
-     * @return translate result.
-     */
-    public static String request(String requestUrl) {
-        return Cmd.execute(false, new String[]{"curl", "-L", "-s", requestUrl});
-    }
-
+public interface TranslationClient {
     /**
      * translate by text as...
      * 1. text is english -> translate to japanese
@@ -59,15 +8,6 @@ public class TranslationClient {
      *
      * @param text translate text.
      * @return translate result.
-     * @throws LangDetectException see {@link LangDetectorOfCybozuLabs#isJapanese(String)}
      */
-    public static String translate(String text) throws LangDetectException {
-        text = Objects.isNull(text) ? "" : text;
-        String requestUrl = detector.isJapanese(text)
-                ? createRequestUrl(text, TargetLang.JAPANESE, TargetLang.ENGLISH)
-                : createRequestUrl(text, TargetLang.ENGLISH, TargetLang.JAPANESE);
-
-        return request(requestUrl);
-    }
-
+    String translate(String text);
 }
---
+++ src/translate/TranslationClientOfGoogleAppScript.java
@@ -0,0 +1,65 @@
+package translate;
+
+import app.Setting;
+import com.cybozu.labs.langdetect.LangDetectException;
+import org.apache.commons.codec.net.URLCodec;
+import tools.Cmd;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Objects;
+
+/**
+ * see https://script.google.com/home/projects/1sFWfV_MOJbmBeTLT_R9F4Qq0qPolgkRsVt8A_sCMI2C9DTccZBRdHFDt/edit
+ */
+public class TranslationClientOfGoogleAppScript implements TranslationClient{
+    private static final URLCodec codec = new URLCodec("UTF-8");
+
+    private final LangDetector detector = new LangDetectorOfCybozuLabs();
+
+    public TranslationClientOfGoogleAppScript() {
+    }
+
+    /**
+     * create request URL as google apps script as "google_translate_api".
+     *
+     * @param text   translate text
+     * @param source before language
+     * @param target after language
+     * @return request URL as google apps script as "google_translate_api"
+     */
+    public static String createRequestUrl(String text, TargetLang source, TargetLang target) {
+        try {
+            text = Objects.isNull(text) ? "" : codec.encode(text, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            if (Setting.getAsBoolean("debug_mode")) {
+                e.printStackTrace();
+            }
+        }
+
+        String url = Setting.getAsString("translate_request_url");
+        return url.replace("{text}", text)
+                .replace("{source}", source.languageCode)
+                .replace("{target}", target.languageCode);
+    }
+
+    /**
+     * translate by URL.
+     *
+     * @param requestUrl created by {@link TranslationClientOfGoogleAppScript#createRequestUrl(String, TargetLang, TargetLang)}.
+     * @return translate result.
+     */
+    public static String request(String requestUrl) {
+        return Cmd.execute(false, new String[]{"curl", "-L", "-s", requestUrl});
+    }
+
+    @Override
+    public String translate(String text){
+        text = Objects.isNull(text) ? "" : text;
+        String requestUrl = detector.isJapanese(text)
+                ? createRequestUrl(text, TargetLang.JAPANESE, TargetLang.ENGLISH)
+                : createRequestUrl(text, TargetLang.ENGLISH, TargetLang.JAPANESE);
+
+        return request(requestUrl);
+    }
+
+}
