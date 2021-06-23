6a09aac72d68fc31dd6ebd3b6e8b39fe6fb6b1ea
monkey999por-sub <85392145+monkey999por-sub@users.noreply.github.com>
2021-06-23 09:42:05 +0900

    clipboard getting by specified flavor or infer

--- src/thread/ClipBoardObserver.java
+++ src/thread/ClipBoardObserver.java
@@ -4,30 +4,38 @@ import app.Setting;
 import tools.MyClipBoard;
 import translate.TranslationWorker;
 
+import java.awt.datatransfer.DataFlavor;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Objects;
+
 /**
  * Monitor System clipboard.
  */
 public class ClipBoardObserver implements Runnable {
 
-    //前回のクリップボードの内容を保持する
-    private static String lastTimeClipText = MyClipBoard.getText() == null ? "" : MyClipBoard.getText();
-
     TranslationWorker worker = new TranslationWorker();
 
-    public ClipBoardObserver() {
-    }
+    //前回のクリップボードの内容を保持する
+    private static Object lastTimeClip = MyClipBoard.get();
 
     /**
      * Monitor System clipboard, and when detect clipboard changed, run event.
      */
     @Override
     public void run() {
+
         // クリップボードを監視しながらポーリング
         int gcCount = 0;
         var loopInterval = Setting.getAsLong("loop_interval");
 
         while (true) {
-            if (lastTimeClipText.equals(MyClipBoard.getText())) {
+
+            var currentClip = MyClipBoard.get();
+            if (lastTimeClip != null &&
+                currentClip != null &&
+                    lastTimeClip.equals(MyClipBoard.get())) {
                 //一定時間経過後にGCする 1800ループごと
                 if (gcCount++ > 1800) {
                     System.out.println("Garbage Collection called");
@@ -37,17 +45,15 @@ public class ClipBoardObserver implements Runnable {
                 try {
                     Thread.sleep(loopInterval);
                 } catch (InterruptedException e) {
-                    if (Setting.getAsBoolean("debug_mode")) {
-                        e.printStackTrace();
-                    }
+                    if (Setting.getAsBoolean("debug_mode")) e.printStackTrace();
                 }
                 continue;
             }
 
             // translation worker run
-            var ct = MyClipBoard.getText();
+            var ct = (String) MyClipBoard.get(DataFlavor.stringFlavor);
             worker.run(ct);
-            lastTimeClipText = ct;
+            lastTimeClip = ct;
         }
     }
 }
--- src/tools/MyClipBoard.java
+++ src/tools/MyClipBoard.java
@@ -1,29 +1,67 @@
 package tools;
 
+import app.Setting;
+
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * System clipboard wrapper.
  */
 public class MyClipBoard {
     //クリップボード
-    private static final Toolkit kit = Toolkit.getDefaultToolkit();
-    private static final Clipboard clip = kit.getSystemClipboard();
+    private static Toolkit kit;
+    private static Clipboard clip;
 
     /**
-     * get text from saved clipboard.
-     *
-     * @return text
+     * init system clipboard
      */
-    public static String getText() {
+    private static void init() {
+        kit = Toolkit.getDefaultToolkit();
+        clip = kit.getSystemClipboard();
+    }
+
+    /**
+     * get data from saved clipboard.
+     * get value by cast of {@link DataFlavor} supported.
+     *
+     * @return The current clipboard value is the specified flavor to return current clipboard.
+     * not is null.
+     */
+    public static Object get(DataFlavor flavor) {
         try {
-            return (String) clip.getData(DataFlavor.stringFlavor);
-        } catch (UnsupportedFlavorException | IOException e) {
-            return "";
+            init();
+            return clip.isDataFlavorAvailable(flavor) ? clip.getData(flavor) : null;
+
+        } catch (Exception e) {
+            if (Setting.getAsBoolean("debug_mode")) e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * get current clipboard data.
+     *
+     * @return type of {@link java.lang.String} or {@link java.awt.Image}.
+     */
+    public static Object get() {
+        try {
+            init();
+            DataFlavor availableFlavor = Arrays.asList(
+                    DataFlavor.stringFlavor,
+                    DataFlavor.imageFlavor).stream()
+                    .filter(f -> clip.isDataFlavorAvailable(f))
+                    .findFirst()
+                    .get();
+
+            return clip.getData(availableFlavor);
+        } catch (Exception e) {
+            if (Setting.getAsBoolean("debug_mode")) e.printStackTrace();
+            return null;
         }
     }
 }
