f8c59223ddfdf91e0a961f2bbbd48c27ddb84a0f
monkey999por <34706709+monkey999por@users.noreply.github.com>
2023-02-18 20:26:25 +0900

    ej.bat

--- ej.bat
+++ ej.bat
@@ -15,11 +15,16 @@ if "%1"=="-v2" goto :VERSION2
 
 :LATEST
 start "translation" /WAIT /B java -Dfile.encoding="utf-8" -jar .\target\translation-0.0.1-SNAPSHOT-jar-with-dependencies.jar .\setting.properties
+goto END
 
 :VERSION2
 start "translation" /WAIT /B java -Dfile.encoding="utf-8" -jar C:\tools\translation\translation-0.0.1-SNAPSHOT_v2.jar C:\tools\translation\setting.properties
+pause
+GOTO END
 
 ::echo "Restart..."
 ::timeout /T 2
 ::goto RESTART
 
+
+:END
